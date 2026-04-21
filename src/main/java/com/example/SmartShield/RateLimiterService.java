aca645ef1f97d377faad103c164e3710f424250c
MAffanG <maghori2006@gmail.com>
2026-04-21 23:35:33 +0530

    Improve rate limiting key and refine service structure

--- src/main/java/com/example/SmartShield/RateLimiterService.java
+++ src/main/java/com/example/SmartShield/RateLimiterService.java
@@ -24,13 +24,19 @@ public class RateLimiterService {
         //Read annotation
         RateLimited rateLimited=handlerMethod.getMethodAnnotation(RateLimited.class);
 
-        //No annotation  - allow request
-        if(rateLimited==null){
-            return RateLimitResult.allowed();
+        int maxRequests;
+        long windowMs;
+        //No annotation  - setting a default value for the request
+        if(rateLimited!=null){
+            maxRequests= rateLimited.maxRequests();
+            windowMs=rateLimited.windowMs();
+        }
+        else{
+            maxRequests=6;
+            windowMs=60000;
         }
 
-        int maxRequests= rateLimited.maxRequests();
-        long windowMs=rateLimited.windowMs();
+
 
         long now=System.currentTimeMillis();
         long windowStart=now-windowMs;
@@ -39,7 +45,7 @@ public class RateLimiterService {
 //        String ip=request.getRemoteAddr();
         String ip=clientIpResolver.getClientIp(request);
         String path=request.getRequestURI();
-        String key=ip+":"+path;
+        String key=ip+":"+request.getMethod()+":"+path;
         Deque<Long> timestamps=requestTimestamps.computeIfAbsent(key,k->new ConcurrentLinkedDeque<>());
 
         //remove old requests ie.expired
--- src/main/java/com/example/SmartShield/controller/TestController.java
+++ src/main/java/com/example/SmartShield/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.SmartShield.controller;
 
 import com.example.SmartShield.RateLimited;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,14 +18,21 @@ public class TestController {
     @RateLimited(maxRequests = 10,windowMs = 120000)
     @GetMapping("/login")
     public String login(){
-        System.out.println("Login hit!");
-        return "Login Page";
+        System.out.println("(GET)Login hit!");
+        return "(GET)Login Page";
     }
 
     @GetMapping("/dev")
     public String dev(){
         System.out.println("Dev hit!");
         return "Dev Page";
+    }
+
+    @RateLimited(maxRequests = 10,windowMs = 120000)
+    @PostMapping("/login")
+    public String pLogin(){
+        System.out.println("(POST)Login hit!");
+        return "(POST)Login Page";
     }
 
 }
